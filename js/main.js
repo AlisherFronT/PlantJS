f3a63cc81665bc247b96e7b08ce5fefeba3eea84
EsengulovAlmaz <118911399+EsengulovAlmaz@users.noreply.github.com>
2023-01-29 17:44:50 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ let type = ''
 // Вывод продукта из базы
 const getPlants = () => {
     plantList.innerHTML = ''
-    fetch(url + `${price.length ? '_sort=price&_order='+ price + '&' : ''}${type.length ? 'type='+ type : ''}`)
+    fetch(url + `${price.length ? '_sort=price&_order=' + price + '&' : ''}${type.length ? 'type=' + type : ''}`)
         .then((res) => res.json())
         .then((res) => {
             res.forEach((item) => {
@@ -47,6 +47,58 @@ filterType.addEventListener('change', (e) => {
 })
 
 
-
-
 getPlants();
+
+
+
+//User
+
+const user = document.querySelector(".user");
+const userName = document.querySelector(".userName");
+const userEmail = document.querySelector(".userEmail");
+const userImg = document.querySelector(".userImg");
+const userInfo = document.querySelector(".userInfo");
+const logOut = document.querySelector(".logOut");
+
+window.addEventListener("load", () => {
+    const user = JSON.parse(localStorage.getItem("currentUser"));
+
+    userName.innerHTML += user.name;
+    userEmail.innerHTML += user.email;
+    userImg.src = user.image;
+
+    if (localStorage.getItem("access_token") === "false") {
+        window.open("../pages/auth.html", "_self")
+    }
+})
+
+user.addEventListener("click", (e) => {
+    e.preventDefault()
+    userInfo.classList.toggle("active");
+    user.classList.toggle("userIcon");
+})
+
+logOut.addEventListener("click", () => {
+    localStorage.setItem("access_token", "false");
+    window.open("../pages/auth.html", "_self")
+})
+
+
+
+/*swiper*/
+new Swiper(".swiper", {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+    },
+    grabCursor: true,
+    mousewheel: {
+        sensitivity: 1,
+    },
+    slidesPerView: 3,
+    loop: true,
+})
